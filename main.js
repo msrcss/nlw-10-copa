ee1cc7ececacd28393f407fb943820db3e013383
Marcus Vinicius <marcusvim7004b@gmail.com>
2022-11-04 11:58:39 -0300

    :tada: initial commit

---
+++ main.js
@@ -0,0 +1,49 @@
+function createGame(player1, hour, player2) {
+    return `
+    <li>
+        <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}">
+        <strong>${hour}</strong>
+        <img src="./assets/icon-${player2}.svg" alt="Bandeira da ${player2}">
+    </li>
+    `
+}
+
+let delay = -0.3;
+function createCard(date, day, games) {
+    delay = delay + 0.3;
+    return `
+    <div class="card" style="animation-delay: ${delay}s">
+        <h2>${date} <span>${day}</span></h2>
+
+            <ul>
+                ${games}
+            </ul>
+    </div>
+    `    
+}
+
+
+document.querySelector('#cards').innerHTML = 
+
+        createCard('23/11', 'terça', 
+        createGame('germany', '09:00', 'japan') + createGame('spain', '12:00', 'costa-rica') + createGame ('belgium', '15:00', 'canada')) 
+
+        +
+
+        createCard('24/11', 'quinta', 
+        createGame('switzerland', '07:00', 'cameroon') + createGame('portugal', '13:00', 'ghana') + createGame('brazil', '16:00', 'serbia')) 
+
+        +
+
+        createCard('28/11', 'segunda',
+        createGame('south-korea', '07:00', 'ghana') + createGame('brazil', '13:00', 'switzerland') + createGame('portugal', '16:00', 'uruguay'))
+
+        +
+
+        createCard('02/12', 'sexta',
+        createGame('south-korea', '11:00', 'portugal') + 
+        createGame('serbia', '15:00', 'switzerland') + 
+        createGame('cameroon', '15:00', 'brazil'))
+
+
+         
